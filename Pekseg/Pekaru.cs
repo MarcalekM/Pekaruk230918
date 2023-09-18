e1db39a2677aaf0609ad71a3a499e7c4cf14f622
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-18 11:15:18 +0200

    Add files via upload

---
+++ Pekseg/Pekaru.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pekseg
+{
+    class Pekaru
+    {
+        public string Nev { get; set; }
+        public float Zsir { get; set; }
+        public float Szenhidrat { get; set; }
+
+        public Pekaru(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+            Zsir = float.Parse(v[1]);
+            Szenhidrat = float.Parse(v[2]);
+        }
+    }
+}
---
+++ Pekseg/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Pekseg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pekaruk = new List<Pekaru>();
+            var sr = new StreamReader(
+                )
+        }
+    }
+}

3684fdd46948325972fcc53143ddec0b7dce4045
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-18 11:20:44 +0200

    Beolvasás és kiiratás

--- Pekseg/Program.cs
+++ Pekseg/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.IO;
 
 namespace Pekseg
@@ -10,7 +13,13 @@ namespace Pekseg
         {
             var pekaruk = new List<Pekaru>();
             var sr = new StreamReader(
-                )
+                path: @"..\..\..\src\Pekaruk.txt",
+                Encoding.UTF8);
+            while (!sr.EndOfStream) pekaruk.Add(new Pekaru(sr.ReadLine()));
+            foreach (var p in pekaruk)
+            {
+                Console.WriteLine($"\t{p.Nev},\n\t{p.Zsir},\n\t{p.Szenhidrat}\n");
+            }
         }
     }
 }
